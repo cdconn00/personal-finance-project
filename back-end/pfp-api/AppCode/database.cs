b606a39c0d9448bd38845c9a21a8233b4aa64cfc
Cole D. Connelly <cdconn00@gmail.com>
2022-01-07 17:22:05 -0600

    Add basic auth controller, database class.

---
+++ back-end/pfp-api/AppCode/database.cs
@@ -0,0 +1,69 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+using System.Collections;
+using System.Data;
+
+namespace pfp_api.Core
+{
+    public class database
+    {
+        private readonly IConfiguration config;
+        private NpgsqlConnection connection;
+
+        public database(IConfiguration configuration)
+        {
+            config = configuration;
+            connection = new NpgsqlConnection(config["ConnectString"]);
+        }
+
+        public DataSet GetDataSet(string sql, ArrayList? @params = null)
+        {
+            DataSet ds = new DataSet();
+
+            connection.Open();
+
+            try
+            {
+                using (var cmd = new NpgsqlCommand(sql, connection))
+                {
+                    
+                    if (@params != null)
+                    {
+                        foreach (Param itm in @params)
+                        {
+                            cmd.Parameters.AddWithValue(itm.Name, itm.Type, itm.Value);
+                        }
+                    }
+                    
+                    NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(cmd);
+                    ds.Reset();
+                    dataAdapter.Fill(ds);
+                    connection.Close();
+
+                    return ds;
+                    
+                }
+            }
+            catch (NpgsqlException ex)
+            {
+                connection.Close();
+                Console.WriteLine(ex.Message);
+                return ds;
+            }
+        }
+
+        public class Param
+        {
+            public string Name { get; set; }
+            public NpgsqlDbType Type { get; set; }
+            public object Value { get; set; }
+
+            public Param(string name, NpgsqlDbType type, object value)
+            {
+                Name = name;
+                Type = type;
+                Value = value;
+            }
+        }
+    }
+}
---
+++ back-end/pfp-api/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NpgsqlTypes;
+using pfp_api.Core;
+using System.Collections;
+using System.Data;
+
+namespace pfp_api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration Configuration;
+        private database db;
+
+        public AuthController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            db = new database(Configuration);
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            ArrayList p = new ArrayList
+            {
+                new database.Param("test", NpgsqlDbType.Integer, 1)
+            };
+
+            DataSet ds = db.GetDataSet("SELECT 1 = @test", p);
+
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
